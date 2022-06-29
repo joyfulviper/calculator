cd5aa310530a4b788470ec1ccd59424d59c8c25b
doyeon <kimsk3113@naver.com>
2022-06-29 16:45:48 +0900

    두 번째 수정

--- src/calculator/CalculatorResult.java
+++ src/calculator/CalculatorResult.java
@@ -1,6 +1,5 @@
 package calculator;
 
-import java.util.Scanner;
 
 public class CalculatorResult {
 
--- src/calculator/PlusCalculate.java
+++ src/calculator/PlusCalculate.java
@@ -1,6 +1,6 @@
 package calculator;
 
-public class PlusCalculate implements Calculator { //다양한 계산기 구현 다형성
+public class PlusCalculate implements Calculator {
 
     @Override
      public double calculate(double a, double b) {
